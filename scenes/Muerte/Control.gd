1a9b5c54852c4318a0a1388c865bbeb2d9c01da3
ChuchuGiampo <chulogiamportone@gmail.com>
2024-01-27 19:34:59 -0300

    muerte vuelve al juego

--- scenes/Muerte/Control.gd
+++ scenes/Muerte/Control.gd
@@ -1,3 +1,8 @@
 extends Control
 
 
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/nueva_ciudad.tscn")
+	pass # Replace with function body.
--- scenes/Splash/Control.gd
+++ scenes/Splash/Control.gd
@@ -4,11 +4,11 @@ extends Control
 
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/ciudad.tscn")
+	get_tree().change_scene_to_file("res://scenes/nueva_ciudad.tscn")
 	pass # Replace with function body.
 
 
 func _on_button_2_pressed():
-	queue_free()
+	get_tree().quit()
 	pass # Replace with function body.
 
